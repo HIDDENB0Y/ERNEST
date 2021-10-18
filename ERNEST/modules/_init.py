000580318bc05373eb8124609af3a8e5321d67c5
Jatindalal875 <79321850+Jatindalal875@users.noreply.github.com>
2021-10-18 13:49:05 +0530

    Update _init.py

--- ERNEST/modules/_init.py
+++ ERNEST/modules/_init.py
@@ -1,1 +1,7 @@
+#THE CODES IN THIS PROJECT i.e ERNEST GROUP MANEGEMENT BOT ARE ASSOCIATED TO NONE OTHER THAN ITS THE LEAVING_TG CLAN
+Mr-Ernest-helper/ERNEST is licensed under the
 
+GNU General Public License v3.0
+Permissions of this strong copyleft license are conditioned on making available complete source code of licensed works and modifications, 
+which include larger works using a licensed work, under the same license. Copyright and license notices must be preserved.
+Contributors provide an express grant of patent rights.

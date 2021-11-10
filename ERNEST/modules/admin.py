4febf32e6d97ca41d2e7bcbfa889702aa8c00cc3
SerpentsDev <92170520+AjayReddytheNoobDev@users.noreply.github.com>
2021-11-10 14:33:55 +0530

    Update admin.py

--- ERNEST/modules/admin.py
+++ ERNEST/modules/admin.py
@@ -5,7 +5,7 @@ from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
 from telegram.utils.helpers import mention_html
 
-from ERNESTRoBot import CHACHA VIDHAYAK, dispatcher
+from ERNESTRoBot import DRAGONS, dispatcher
 from ERNESTRoBot.modules.disable import DisableAbleCommandHandler
 from ERNESTRoBot.modules.helper_funcs.chat_status import (
     bot_admin,
